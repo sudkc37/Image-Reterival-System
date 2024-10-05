c794b625f165f45971498e45e12d1df09c21c345
Sudip Khadka <sudipkhadka@Sudips-MBP.lan>
2024-10-04 22:20:21 -0500

    custom model

---
+++ .ml/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/sudipkhadka/Desktop/mlop/semantic_search/.ml/bin/python3.12
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # semantic_search
+
+#we are building three model; our own model, resnet and VGG16
+#Use cosine similarity to extract the similar vector. 
